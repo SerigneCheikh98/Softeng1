58656cc109f7c9e59f737bb86ba79371f23a556d
DOMENICO GAGLIARDO s310454 <s310454@studenti.polito.it>
2023-05-27 16:54:38 +0200

    2 fix VerifyAuth

--- code/controllers/utils.js
+++ code/controllers/utils.js
@@ -127,7 +127,7 @@ export const verifyAuth = (req, res, info) => {
                     in_group = true;
                 }
             }
-            if (in_group === false && decodedAccessToken.role !== 'Admin') {
+            if (in_group === false) {
                 return { authorized: false, cause: "Group: user not in group" };
             }
         }
