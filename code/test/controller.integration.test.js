046404abd756b781d7e2c1810788919274fed26d
DOMENICO GAGLIARDO s310454 <s310454@studenti.polito.it>
2023-06-04 10:14:59 +0200

    added API.md and TestReport.md

--- code/test/controller.integration.test.js
+++ code/test/controller.integration.test.js
@@ -1,6 +1,9 @@
 import request from 'supertest';
 import { app } from '../app';
+import { User, Group } from '../models/User.js';
 import { categories, transactions } from '../models/model';
+import jwt from 'jsonwebtoken';
+const bcrypt = require("bcryptjs")
 import mongoose, { Model } from 'mongoose';
 import dotenv from 'dotenv';
 
@@ -21,6 +24,38 @@ afterAll(async () => {
   await mongoose.connection.db.dropDatabase();
   await mongoose.connection.close();
 });
+
+//necessary setup to ensure that each test can insert the data it needs
+beforeEach(async () => {
+    await categories.deleteMany({})
+    await transactions.deleteMany({})
+    await User.deleteMany({})
+    await Group.deleteMany({})
+  });
+  
+/**
+ * Alternate way to create the necessary tokens for authentication without using the website
+ */
+const adminAccessTokenValid = jwt.sign({
+    email: "admin@email.com",
+    //id: existingUser.id, The id field is not required in any check, so it can be omitted
+    username: "admin",
+    role: "Admin"
+}, process.env.ACCESS_KEY, { expiresIn: '1y' })
+
+const testerAccessTokenValid = jwt.sign({
+    email: "tester@test.com",
+    username: "tester",
+    role: "Regular"
+}, process.env.ACCESS_KEY, { expiresIn: '1y' })
+
+//These tokens can be used in order to test the specific authentication error scenarios inside verifyAuth (no need to have multiple authentication error tests for the same route)
+const testerAccessTokenExpired = jwt.sign({
+    email: "tester@test.com",
+    username: "tester",
+    role: "Regular"
+}, process.env.ACCESS_KEY, { expiresIn: '0s' })
+const testerAccessTokenEmpty = jwt.sign({}, process.env.ACCESS_KEY, { expiresIn: "1y" })
 
 describe("createCategory", () => { 
     test('Dummy test, change it', () => {
